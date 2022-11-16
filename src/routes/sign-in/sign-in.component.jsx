c95ebc69d869cc95ee6eb8d008096cd4cfc7bce7
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2022-11-16 16:09:52 +0100

    Delete signInWithGoogleRedirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,24 +1,9 @@
-import {useEffect} from 'react'
-import {getRedirectResult} from 'firebase/auth'
 import {
-    auth,
     signInWithGooglePopup,
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await getRedirectResult(auth)
-            if (response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user)
-            }
-        }
-        fetchData()
-            .catch(console.error);
-    }, [])
-
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -28,7 +13,6 @@ const SignIn = () => {
         <div>
             <h1>Sign in Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 }
